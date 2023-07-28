4bd67ecaab892ef1ebc79aca3f46713433092060
Ma-Kas <115688479+Ma-kas@users.noreply.github.com>
2023-07-28 17:30:33 +0900

    Complete Design and HTML, CSS Setup
    Hardcode App layout and placeholders;
    Need to implement fetching and populating API data;
    Need to add horizontal scroll on overflow hourly cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
+import { format } from "date-fns";
 import "../css/style.css";
+
+async function getWeatherData(lookupLocation) {
+  const location = lookupLocation;
+  // const ipLocation = 'auto:ip';
+  const apiResponse = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=7dfdd5c7ad294aa294643209232707&q=${location}&days=3&aqi=no&alerts=no`,
+    { mode: "cors" },
+  );
+  const weatherData = await apiResponse.json();
+  console.log(weatherData.forecast.forecastday[0].hour[11]);
+}
+
+getWeatherData("osaka");
